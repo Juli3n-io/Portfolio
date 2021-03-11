95f44d122c1083f1499474d1449ae109a1c06e2d
Julien QUENTIER <j.quentier@gmail.com>
2021-03-11 19:32:18 +0100

    Création de la partie Pricing
    Création de la partie Pricing | création du dossier view | déplacement des sections dans la partie views

---
+++ assets/views/price.php
@@ -0,0 +1,59 @@
+<section class="pricing" id="pricing">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-sm-12 text-center text-white heading">
+          <h2>Tarifs</h2>
+        </div>
+
+        <div class="col-md-4">
+          <div class="pricing dark-bg">
+            <i class="fa fa-calendar"></i>
+            <h4>Taux Journalier</h4>
+            <p>Je suis disponible a temps complet</p>
+            <h5>250€*</h5>
+            <ul>
+              <li>Développement Web</li>
+              <li>Intégration Web</li>
+              <li>Freelance</li>
+            </ul>
+            <a href="" class="btn custom-btn">Engager moi</a>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="pricing dark-bg">
+            <i class="fas fa-store"></i>
+            <h4>Site Vitrine</h4>
+            <p>Je réalise votre site sur mesure</p>
+            <h5>500€*</h5>
+            <ul>
+              <li>Developpement sur mesure</li>
+              <li>Personalisé</li>
+              <li>avec Hébergement</li>
+            </ul>
+            <a href="" class="btn custom-btn">Engager moi</a>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="pricing dark-bg">
+            <i class="fas fa-shopping-cart"></i>
+            <h4>E-Commerce</h4>
+            <p>Je créer votre boutique en ligne</p>
+            <h5>1500€*</h5>
+            <ul>
+            <li>Developpement sur mesure</li>
+              <li>Personalisé</li>
+              <li>avec Hébergement</li>
+            </ul>
+            <a href="" class="btn custom-btn">Engager moi</a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <p class="condition">* Prix HT, TVA non applicable, art. 293B du CGI</p>
+
+  </section>
--- index.php
+++ index.php
@@ -5,11 +5,13 @@ $page_title ='';
 
 include __DIR__. '/assets/includes/header.php';
 
-include __DIR__. '/assets/includes/hero.php';
+include __DIR__. '/assets/views/hero.php';
 
-include __DIR__. '/assets/includes/about.php';
+include __DIR__. '/assets/views/about.php';
 
-include __DIR__. '/assets/includes/services.php';
+include __DIR__. '/assets/views/services.php';
+
+include __DIR__. '/assets/views/price.php';
 
 include __DIR__. '/assets/includes/footer.php';
 ?>
